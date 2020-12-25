3bc14a11d49ea40487d1b1a8e346e168816ea586
LvMuYing <3430163455@qq.com>
2020-12-25 14:48:07 +0800

    yidu

---
+++ src/com/yidu/Main.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @description:
+ * @author: LvFang
+ * date:2020/12/25 14:46
+ * version:1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("一度教育");
+    }
+}
